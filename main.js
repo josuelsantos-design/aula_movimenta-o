9c21ed3d0d9384c84cede12adfc5d5e57dba17b1
josuelsantos-design <josuel.santos@aluno.educa.go.gov.br>
2026-02-19 14:01:12 +0000

    criou codigo inicial de um jogo

--- main.js
+++ main.js
@@ -1,1 +1,39 @@
+const canvas = document.getElementById("jogoCanvas")
+const ctx = canvas.getContext("2d")
 
+ //Config. personagem
+let player = {
+    x:360,
+    y:0,
+    tamanho:40,
+    velocidade:5,
+    cor: "black"
+};
+
+// monitor de teclas
+let teclas = {};
+window.addEventListener("keydown", (e) => teclas[e.key] = true);
+window.addEventListener("keyup", (e) => teclas[e.key] = false);
+
+function atualizar()
+{
+//movimentação no eixo x
+if(teclas["ArrowLeft"]) player.x -= player.velocidade;
+if(teclas["ArrowRight"]) player.x += player.velocidade;
+//Movimentação no eixo y
+if(teclas["ArrowDown"]) player.y += player.velocidade;
+if(teclas["ArrowUp"]) player.y -= player.velocidade;
+}
+// Limpar a tela e criar o personagem
+function desenhar(){
+ctx.clearRect(0,0, canvas.width, canvas.height);
+ctx.fillStyle = player.cor;
+ctx.fillRect (player.x, player.y, player.tamanho, player.tamanho);
+}
+//iniciar o jogo
+function loop(){
+    atualizar();
+    desenhar();
+    requestAnimationFrame(loop);
+}
+loop();
