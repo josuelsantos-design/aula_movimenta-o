d1276e46fa6d8fd694cfd30066a14f4e52d16e6e
josuelsantos-design <josuel.santos@aluno.educa.go.gov.br>
2026-02-26 11:14:54 -0300

    Enhance game logic and player controls
    Refactor player movement and game mechanics.

--- main.js
+++ main.js
@@ -1,23 +1,64 @@
-const canvas = document.getElementById("jogoCanvas")
-const ctx = canvas.getContext("2d")
 
- //Config. personagem
-let player = {
-    x:360,
-    y:0,
-    tamanho:40,
-    velocidade:5,
-    cor: "black"
+
+const canvas = document.getElementById("jogoCanvas");
+const ctx = canvas.getContext("2d");
+const spanPlacar = document.getElementById("placar");
+
+let pontucao = 0;
+let frames = 0;
+let jogoAtivo = true;
+let velocidadeGlobal = 3;
+let frenquenciaCriacao = 60;
+
+let player ={
+ x: 180,
+ y: 430,
+ tamanho: 35,
+ velocidade: 6,
+ cor: "blue"
 };
 
-// monitor de teclas
+let obstaculos [];
+
 let teclas = {};
 window.addEventListener("keydown", (e) => teclas[e.key] = true);
 window.addEventListener("keyup", (e) => teclas[e.key] = false);
 
-function atualizar()
-{
-//movimentação no eixo x
+function gerenciarMovimento (){
+ if(teclas["ArrowLeft"] && player.x > 0){
+  player.x -= player.velocidade;
+ }
+ if(teclas["ArrowRight"] && player.x + player.tamanho < canvas.width){
+   player.x += player.velocidade;
+ }
+  if(teclas["ArrowUp"] && player.x > 0){
+  player.y -= player.velocidade;
+ }
+ if(teclas["ArrowDown"] && player.x + player.tamanho < canvas.heidth){
+   player.y += player.velocidade;
+ }
+}
+
+setInterval(()=>{
+  if(jogoAtivo){
+    velocidadeGlobal += 1;
+    console.log("Aumentando velocidade base para: " + velocidadeGlobal)
+  }
+}, 30000) 
+
+function atualizar(){
+  frames++;
+ if(frames % frequenciaCriacao === 0)
+   obstaculos.push({
+     x: Math.random() * (canvas.width - 25),
+     y: -30,
+     tamanho: 25,
+     cor: "#e74c3c"
+   });
+
+}
+
+function atualizar(){
 if(teclas["ArrowLeft"]) player.x -= player.velocidade;
 if(teclas["ArrowRight"]) player.x += player.velocidade;
 //Movimentação no eixo y
